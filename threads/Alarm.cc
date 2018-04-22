820898f6b626a16840b57a9645b8ec4f12457e1d
aksudya <467498869@qq.com>
2018-04-22 23:40:15 +0800

    实现alarm

--- threads/Alarm.cc
+++ threads/Alarm.cc
@@ -17,12 +17,16 @@ Alarm::~Alarm()
 
 void Alarm::Pause(int howLong)
 {
+	IntStatus oldLevel = interrupt->SetLevel(IntOff);	//必须关中断
+
 	waiters++;
 	queue->SortedInsert((void *)currentThread,
 		stats->totalTicks + howLong);	//按结束时间依序插入链表
 
 	//queue->Append((void *)currentThread);
 	currentThread->Sleep();
+
+	(void)interrupt->SetLevel(oldLevel);
 }
 
 
@@ -30,6 +34,9 @@ void timerhandler(int dummy)		//dummy 仅为占位，不需要用到这个参数
 {
 	int duetime;
 	Thread *thread;
+
+	IntStatus oldLevel = interrupt->SetLevel(IntOff);	//必须关中断
+
 	thread = (Thread *)alarm->queue->Remove(&duetime);
 	while (thread!= NULL)
 	{
@@ -45,5 +52,6 @@ void timerhandler(int dummy)		//dummy 仅为占位，不需要用到这个参数
 				stats->totalTicks - duetime, alarm->waiters);
 		}
 	}
-	
+
+	(void)interrupt->SetLevel(oldLevel);
 }
