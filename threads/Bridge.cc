436330648f6267bcefc7e1aa2b614a701607f0ef
aksudya <467498869@qq.com>
2018-04-26 10:35:22 +0800

    开始写birdge 红绿灯算法

--- threads/Bridge.cc
+++ threads/Bridge.cc
@@ -3,7 +3,7 @@
 
 extern Alarm *alarm;
 
-#ifdef FCFS
+#ifdef FCFS		//先来先服务算法
 
 Bridge::Bridge()
 {
@@ -60,6 +60,60 @@ void Bridge::ExitBridge(int direc)
 
 	lock->Release();
 
+}
+
+
+
+#endif
+
+
+#ifdef TRAFFIC_LIGHT	//红绿灯算法
+Bridge::Bridge()
+{
+	lock = new Lock("bridge lock");
+	direc_0_con = new Condition("direc 0 condition");
+	direc_1_con = new Condition("direc 1 condition");
+
+	on_bridge_num = 0;
+	current_direc = 0;		//默认0方向先行
+
+	direc_0_time = 0;
+	direc_1_time = BASE_RED_LIGHT_TIME;
+
+	direc_0_car_num = 0;
+	direc_1_car_num = 0;
+}
+
+
+
+Bridge::~Bridge()
+{
+	delete lock;
+	delete direc_0_con;
+	delete direc_1_con;
+}
+
+void Bridge::ArriveBridge(int direc)
+{
+	lock->Acquire();
+
+
+
+	lock->Release();
+}
+
+void Bridge::CrossBridge(int direc)
+{
+	printf("%s car is crossing the bridge in %d direc\n%d cars on the bridge\n"
+		, currentThread->getName(), direc, on_bridge_num);
+	alarm->Pause(CROSS_BRIDGE_TIME);
+}
+
+void Bridge::ExitBridge(int direc)
+{
+	lock->Acquire();
+
+	lock->Release();
 }
 
 
--- threads/Bridge.h
+++ threads/Bridge.h
@@ -3,9 +3,11 @@
 #include "synch.h"
 #define CROSS_BRIDGE_TIME 10
 
-#define FCFS
+//#define FCFS			//先来先服务算法
+#define TRAFFIC_LIGHT	//红绿灯算法
 
-#ifdef FCFS
+
+#ifdef FCFS				//先来先服务算法
 
 class Bridge
 {
@@ -30,5 +32,42 @@ private:
 	Condition *con;
 
 };
+
+#endif
+
+#ifdef TRAFFIC_LIGHT	//红绿灯算法
+
+#define BASE_RED_LIGHT_TIME (10*CROSS_BRIDGE_TIME)		//基准红灯时间,为服务时间的十倍
+
+class Bridge
+{
+public:
+	Bridge();
+	~Bridge();
+	void OneVehicle(int direc)
+	{
+		ArriveBridge(direc);
+		CrossBridge(direc);
+		ExitBridge(direc);
+	};
+private:
+	void ArriveBridge(int direc);
+	void CrossBridge(int direc);
+	void ExitBridge(int direc);
+
+	int on_bridge_num;
+	int current_direc;
+
+	Lock *lock;
+	Condition *direc_0_con;
+	Condition *direc_1_con;
+
+	int direc_0_time;
+	int direc_1_time;
+
+	int direc_0_car_num;
+	int direc_1_car_num;
+};
+
 
 #endif
